2efa2dfe4f9495bb288e602c1086448d989b0bc8
ErikHo <f.g.a.homburg@gmail.com>
2017-11-18 17:45:51 +0100

    Add files via upload

---
+++ examples/blinkBurstDemo.ino
@@ -0,0 +1,12 @@
+blinkBurst myBurst;        // create mYBurst instance of the blinkBurst class
+
+void setup() {
+  // 4 times a flash of 50 ms separated by 100 ms, all of this separated by 1300 ms
+  // total cycle time 2000 ms
+  myBurst.begin(LED_BUILTIN, 4, 50, 100, 1300);
+}
+
+void loop() {              // execute main loop
+  myBurst.doBurst();
+}
+
